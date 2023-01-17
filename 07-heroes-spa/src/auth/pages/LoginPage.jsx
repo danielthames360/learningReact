7f7f45064581b90e98ca0b4bcc2f21f3d5eb2009
Daniel Cespedes <danielthames360@gmail.com>
2023-01-17 07:54:08 -0400

    finishing heroes spa

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,10 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
+import { useContext } from "react";
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate("/marvel", {
+    const lastPath = localStorage.getItem("lastPath") || "/";
+
+    login("Daniel");
+
+    navigate(lastPath, {
       replace: true,
     });
   };
@@ -13,7 +20,9 @@ export const LoginPage = () => {
     <div className="container mt-5">
       <h1>Login</h1>
       <hr />
-      <button className="btn btn-primary" onClick={onLogin}>Login</button>
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
     </div>
   );
 };
